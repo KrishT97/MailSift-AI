0fc64762a9d4a713c40a98f5ca319fc5602193c3
Krish Sadhwani <92883393+KrishT97@users.noreply.github.com>
2025-03-02 12:01:05 +0000

    Update user_preference.py

--- modules/user_preference.py
+++ modules/user_preference.py
@@ -1,7 +1,7 @@
 # modules/user_preference.py
 import json
 
-
+# Loading function provided in this file for testing purposes
 def load_user_profile(user_id, feedback_file="data/user_feedback.json"):
     # For simplicity, loading a JSON file where each user has a profile of liked/disliked keywords
     with open(feedback_file, "r") as f:
